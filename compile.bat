d38bc30cbb2fd84d2d466329ce3f570a1e71291c
opertechnic <55390678+opertechnic@users.noreply.github.com>
2023-11-23 00:22:15 +0200

    1st_go

--- compile.bat
+++
@@ -1,1 +0,0 @@
-C:\Users\opert\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\pyinstaller --clean --onefile --icon=ico.ico pcaplay2.py
